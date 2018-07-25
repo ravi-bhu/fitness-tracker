7d5a92107610814e1f9cb08519819212620a72b1
ravi-bhushan-projects <ravibhushan122@gmail.com>
2018-07-25 09:15:20 +0530

    Added Navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { NewTrainingComponent } from './training/new-training/new-training.compo
 import { PastTrainingsComponent } from './training/past-trainings/past-trainings.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import {FlexLayoutModule} from "@angular/flex-layout";
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 
 
 @NgModule({
@@ -25,7 +27,9 @@ import {FlexLayoutModule} from "@angular/flex-layout";
     CurrentTrainingComponent,
     NewTrainingComponent,
     PastTrainingsComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    HeaderComponent,
+    SidenavListComponent
   ],
   imports: [
     BrowserModule,
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,14 +1,36 @@
 import { NgModule } from '@angular/core';
 import {
-  MatButtonModule, MatCheckboxModule,
+  MatButtonModule,
+  MatCheckboxModule,
   MatDatepickerModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatNativeDateModule
+  MatFormFieldModule, MatIconModule,
+  MatInputModule, MatListModule,
+  MatNativeDateModule,
+  MatSidenavModule, MatToolbarModule
 } from "@angular/material";
 
 @NgModule({
-  imports: [ MatFormFieldModule, MatInputModule, MatButtonModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule ],
-  exports: [ MatFormFieldModule, MatInputModule, MatButtonModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule ]
+  imports: [ MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule
+  ],
+  exports: [ MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule
+  ]
 })
 export class MaterialModule { }
